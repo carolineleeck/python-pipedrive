2d663fd9b12384e2457ee07a143778cd75f434ec
Caroline Lee <caroline@chatkit.com>
2018-01-29 20:48:46 -0500

    Replaced HTTPlib2 with requests library

--- pipedrive/__init__.py
+++ pipedrive/__init__.py
@@ -1,10 +1,6 @@
-from httplib2 import Http
 from logging import getLogger
-
-try:
-    from urllib import urlencode
-except ImportError:
-    from urllib.parse import urlencode
+import requests
+from urllib.parse import urlencode
 
 import json
 
@@ -29,40 +25,25 @@ class Pipedrive(object):
         # avoid storing the string 'None' when a value is None
         data = {k: "" if v is None else v for k, v in data.items()}
         if method == "GET":
-            uri = PIPEDRIVE_API_URL + endpoint + '?api_token=' + str(self.api_token)
-            
+            uri = PIPEDRIVE_API_URL + endpoint + '?api_token=' + str(self.api_token)            
             if data:
                 uri += '&' + urlencode(data)
             uri += '&start=' + str(start) + '&limit=' + str(limit)
-            response, data = self.http.request(uri, method=method, headers={'Content-Type': 'application/x-www-form-urlencoded'})
-        else:
+            r = requests.get(uri)
+        elif method == 'POST':
             uri = PIPEDRIVE_API_URL + endpoint + '?api_token=' + str(self.api_token)
-            response, data = self.http.request(uri, method=method, body=urlencode(data), headers={'Content-Type': 'application/x-www-form-urlencoded'})
+            r = requests.post(uri, data=data)
 
         logger.debug('sending {method} request to {uri}'.format(
             method=method,
             uri=uri
         ))
-        # print(json.dumps(json.loads(data.decode('utf-8')), sort_keys=True, indent=4))
+        
+        return r.json()
 
-        # if python2, use:
-        # return json.loads(data)
-        return json.loads(data.decode('utf-8'))
-
-    def __init__(self, email, password=None):
-        self.http = Http()
-        if password:
-            response = self._request("/authorizations/", {"email": email, "password": password})
-
-            if 'error' in response:
-                raise IncorrectLoginError(response)
-
-            # self.api_token = response['authorization'][0]['api_token']
-            self.api_token = response['data'][0]['api_token']
-            print('api_token is ' + self.api_token)
-        else:
-            # Assume that login is actually the api token
-            self.api_token = email
+    def __init__(self, api_token):
+        # Assume that login is actually the api token
+        self.api_token = api_token
 
     def __getattr__(self, name):
         def wrapper(data={}, method='GET', start=0, limit=100, deal_id=None):
